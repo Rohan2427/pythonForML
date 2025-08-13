81501533cd0c89095115bb3b52193a3e39df3ef6
Rohan2427 <rohansonwane8@gmail.com>
2025-08-13 16:53:45 +0530

    task 2

--- numpy/basicConditions.py
+++ numpy/basicConditions.py
@@ -6,3 +6,12 @@ print(m1)
 m1[4] = 1
 print()
 print(m1)
+
+#2. Ask user to input two numbers a, b. Write a program to generate a random array of shape (a, b) and print the array and avg of the array.
+x,y = map(input("Enter two numbers. ").split())
+v1 = np.random.random((x,y))
+print(v1)
+print()
+print(np.mean(v1))
+
+
